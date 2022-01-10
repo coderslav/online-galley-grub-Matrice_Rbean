df4029715ddc72048d904f717e9ce0f4c71796c5
rkomin <kominslav@gmail.com>
2022-01-10 14:38:53 +0100

    Initialization

--- site/routes/root.js
+++ site/routes/root.js
@@ -1,22 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+    res.render('index');
 });
 
-
-router.get('/contact', function(req, res, next) {
-  res.render('contact');
+router.get('/contact', function (req, res, next) {
+    res.render('contact');
 });
 
+router.post('/contact', function (req, res, next) {
+    console.log(req.body);
 
-router.post('/contact', function(req, res, next) {
-  console.log(req.body)
-
-  res.redirect('/contact')
+    res.redirect('/contact');
 });
-
 
 module.exports = router;
