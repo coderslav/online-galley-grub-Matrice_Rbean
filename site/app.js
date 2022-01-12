0073cbc74f0a8584f8de2c079c944b0150a06fff
rkomin <kominslav@gmail.com>
2022-01-12 17:12:59 +0100

    Foods Display

--- site/app.js
+++ site/app.js
@@ -9,15 +9,23 @@ const rootRouter = require('./routes/root');
 const foodsRouter = require('./routes/foods');
 
 const app = express();
-
+let categories;
 
 // view engine setup
-const hbs = exphbs.create({ 
+const hbs = exphbs.create({
     extname: '.hbs',
     helpers: {
-        json: (data) => { return JSON.stringify(data) }
-    }
-})
+        json: (data) => {
+            return JSON.stringify(data);
+        },
+        categoryNameGetter: (id) => {
+            return categories[id - 1].name.toUpperCase();
+        },
+        getCategories: (categoriesFromTemplate) => {
+            categories = categoriesFromTemplate;
+        },
+    },
+});
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
@@ -35,19 +43,19 @@ app.use('/', rootRouter);
 app.use('/foods', foodsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? JSON.stringify(err) : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? JSON.stringify(err) : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
